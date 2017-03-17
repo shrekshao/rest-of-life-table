d90afb4e45038cb9d83a562177ace2b7e7b12579
shrekshao <shrekshao@gmail.com>
2017-03-17 15:15:05 -0400

    basic dynamic table

--- test/js/rlt.js
+++ test/js/rlt.js
@@ -4,15 +4,20 @@ var RLT = RLT || {};
     'use strict'
 
 
+    // RLT.rowTemplateStr = `<tr id="${year}"><td>${year}</td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>`;
+
+    RLT.info = {
+        // username: '',
+        // dateOfBirth: ''
+        lifeSpan: 70,
+        tasks: {
+            '2020/03': 'fuck a girl'
+        }
+    };
 
 
     window.onload = function() {
-        $( "#init-info-form" ).submit( function( event ) {
-            event.preventDefault();
-            console.log(event);
-            // alert( "Handler for .submit() called." );
-            $('#init-modal').modal( 'hide' );
-        });
+        $( "#init-info-form" ).submit( modalFirstTimeSubmit );
 
         $('#datetimepicker').datepicker({
             minViewMode: 'months',
@@ -20,9 +25,20 @@ var RLT = RLT || {};
         });
 
 
+        var localStorageInfo = localStorage.getItem('rltInfo');
+        if (!localStorageInfo) {
+            // assume first usage
+            // pop up init menu
+            console.log('first use');
+            $('#init-modal').modal({
+                backdrop: 'static',
+                keyboard: false
+            });
+        } else {
+            RLT.info = JSON.parse(localStorageInfo);
+            initTablePage();
+        }
 
-
-        initApp();
 
 
         // $("#row-1993").click(function() {
@@ -40,22 +56,59 @@ var RLT = RLT || {};
     };
 
 
-
-    function initApp() {
+    function modalFirstTimeSubmit(event) {
+        event.preventDefault();
+        console.log(event);
         
-        if (!localStorage.getItem('username')) {
-            // assume first usage
-            // pop up init menu
-            console.log('first use');
-            $('#init-modal').modal({
-                backdrop: 'static',
-                keyboard: false
-            });
-        } else {
-        }
+        RLT.info.username = $('#usernameInput').val();
+        var dateOfBirthStr = $('#dateOfBirthInput').val();
+
+        var parts = dateOfBirthStr.split('/')
+
+        RLT.info.dateOfBirth = {
+            year: parseInt(parts[0]),
+            month: parseInt(parts[1])
+        };
+        
+        storeInfo();
+        
+        
+        $('#init-modal').modal( 'hide' );
+
+        initTablePage();
     }
 
 
+
+    function initTablePage() {
+        $('#username').text( RLT.info.username );
+
+        // TODO: build abstract
+
+        // build tables
+        var table = $('#lifeTable');
+        var tbody = table.find('tbody');
+        var yearOfBirth = RLT.info.dateOfBirth.year;
+        var yearOfDeath = yearOfBirth + RLT.info.lifeSpan;
+        // var row;
+        var year, age;
+
+        // var rowTemplate = ;
+
+        for (year = yearOfBirth, age = 0; year <= yearOfDeath; year++, age++) {
+            // row = RLT.rowTemplateStr
+
+            // tbody.append( RLT.rowTemplateStr );
+            tbody.append( `<tr id="${year}"><td>${year}(${age})</td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>` );
+        }
+
+    }
+
+
+
+    function storeInfo() {
+        localStorage.setItem('rltInfo', JSON.stringify(RLT.info));
+    }
 
 
 
